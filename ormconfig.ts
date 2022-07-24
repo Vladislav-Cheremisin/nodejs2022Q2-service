6bfe56f15c9c1529302a4dd0dd65ace49fc9f012
Vladislav Cheremisin <wowjurneo@list.ru>
2022-07-24 06:45:49 +0300

    init: start postgresql task. add ormconfig file

---
+++ ormconfig.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: +process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  logging: true,
+  synchronize: false,
+  migrationsRun: false,
+  migrations: ['dist/migrations/*{.ts,.js}'],
+});
