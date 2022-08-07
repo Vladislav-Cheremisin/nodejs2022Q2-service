094754fe411e794a1715dd1bb719fbb64d9ec319
Vladislav Cheremisin <wowjurneo@list.ru>
2022-08-07 11:33:22 +0300

    feat: add possibility to log into file

--- src/logger/customLogger.ts
+++ src/logger/customLogger.ts
@@ -1,15 +1,45 @@
 import { LoggerService } from '@nestjs/common';
-
+import * as fsPromises from 'fs/promises';
 export class customLogger implements LoggerService {
-  log(message: string) {
-    console.log(message);
+  async log(message: string) {
+    try {
+      await fsPromises.access('./logs');
+      await fsPromises.writeFile(
+        './logs/logs.txt',
+        message + `Level: logs\n\n`,
+        { flag: 'a' },
+      );
+    } catch {
+      await fsPromises.mkdir('./logs');
+      await fsPromises.access('./logs');
+      await fsPromises.writeFile(
+        './logs/logs.txt',
+        message + `Level: logs\n\n`,
+        { flag: 'a' },
+      );
+    }
   }
 
-  error(message: string) {
-    console.log(message);
+  async error(message: string) {
+    try {
+      await fsPromises.access('./logs');
+      await fsPromises.writeFile(
+        './logs/errors.txt',
+        message + `Level: errors\n\n`,
+        { flag: 'a' },
+      );
+    } catch {
+      await fsPromises.mkdir('./logs');
+      await fsPromises.access('./logs');
+      await fsPromises.writeFile(
+        './logs/errors.txt',
+        message + `Level: errors\n\n`,
+        { flag: 'a' },
+      );
+    }
   }
 
-  warn() {
-    console.log('not implemented');
+  async warn(message: string) {
+    console.log(message);
   }
 }
