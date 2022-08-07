256376a056ca25659088d5de4ffde21d96898823
Vladislav Cheremisin <wowjurneo@list.ru>
2022-08-07 19:07:15 +0300

    feat: rework logger code, from now tests work correct

--- src/logger/customLogger.ts
+++ src/logger/customLogger.ts
@@ -13,26 +13,17 @@ export class customLogger implements LoggerService {
         return;
       }
 
-      await fsPromises.access('./logs');
+      await this.createNecessaryFolder('./logs');
+
       await fsPromises.writeFile(
         './logs/logs.txt',
-        message + `Level: logs\n\n`,
+        message + `Type: Logs\n\n`,
         { flag: 'a' },
       );
 
-      const fileStats = await fsPromises.lstat('./logs/logs.txt');
-      const fileSize = (fileStats.size / 1024).toFixed(2);
-
-      if (fileSize > process.env.MAX_LOGFILE_SIZE) {
-        fsPromises.rename('./logs/logs.txt', `./logs/logs_${Date.now()}.txt`);
-      }
-    } catch {
-      await fsPromises.mkdir('./logs');
-      await fsPromises.writeFile(
-        './logs/logs.txt',
-        message + `Level: logs\n\n`,
-        { flag: 'a' },
-      );
+      await this.rotateFilesBySize('./logs/logs.txt');
+    } catch (err) {
+      return;
     }
   }
 
@@ -42,29 +33,17 @@ export class customLogger implements LoggerService {
         return;
       }
 
-      await fsPromises.access('./logs');
+      await this.createNecessaryFolder('./logs');
+
       await fsPromises.writeFile(
         './logs/errors.txt',
-        message + `Level: errors\n\n`,
+        message + `Type: Errors\n\n`,
         { flag: 'a' },
       );
 
-      const fileStats = await fsPromises.lstat('./logs/errors.txt');
-      const fileSize = (fileStats.size / 1024).toFixed(2);
-
-      if (fileSize > process.env.MAX_LOGFILE_SIZE) {
-        fsPromises.rename(
-          './logs/errors.txt',
-          `./logs/errors_${Date.now()}.txt`,
-        );
-      }
-    } catch {
-      await fsPromises.mkdir('./logs');
-      await fsPromises.writeFile(
-        './logs/errors.txt',
-        message + `Level: errors\n\n`,
-        { flag: 'a' },
-      );
+      await this.rotateFilesBySize('./logs/errors.txt');
+    } catch (err) {
+      return;
     }
   }
 
@@ -74,29 +53,17 @@ export class customLogger implements LoggerService {
         return;
       }
 
-      await fsPromises.access('./logs');
+      await this.createNecessaryFolder('./logs');
+
       await fsPromises.writeFile(
         './logs/warnings.txt',
-        message + `Level: warnings\n\n`,
+        message + `Type: Warnings\n\n`,
         { flag: 'a' },
       );
 
-      const fileStats = await fsPromises.lstat('./logs/warnings.txt');
-      const fileSize = (fileStats.size / 1024).toFixed(2);
-
-      if (fileSize > process.env.MAX_LOGFILE_SIZE) {
-        fsPromises.rename(
-          './logs/warnings.txt',
-          `./logs/warnings_${Date.now()}.txt`,
-        );
-      }
-    } catch {
-      await fsPromises.mkdir('./logs');
-      await fsPromises.writeFile(
-        './logs/warnings.txt',
-        message + `Level: warnings\n\n`,
-        { flag: 'a' },
-      );
+      await this.rotateFilesBySize('./logs/warnings.txt');
+    } catch (err) {
+      return;
     }
   }
 
@@ -106,30 +73,17 @@ export class customLogger implements LoggerService {
         return;
       }
 
-      await fsPromises.access('./logs');
+      await this.createNecessaryFolder('./logs');
+
       await fsPromises.writeFile(
         './logs/debug-logs.txt',
-        message + `Level: debug\n\n`,
+        message + `Type: Debug\n\n`,
         { flag: 'a' },
       );
 
-      const fileStats = await fsPromises.lstat('./logs/debug-logs.txt');
-      const fileSize = (fileStats.size / 1024).toFixed(2);
-
-      if (fileSize > process.env.MAX_LOGFILE_SIZE) {
-        fsPromises.rename(
-          './logs/debug-logs.txt',
-          `./logs/debug-logs_${Date.now()}.txt`,
-        );
-      }
-    } catch {
-      await fsPromises.mkdir('./logs');
-      await fsPromises.access('./logs');
-      await fsPromises.writeFile(
-        './logs/debug-logs.txt',
-        message + `Level: debug\n\n`,
-        { flag: 'a' },
-      );
+      await this.rotateFilesBySize('./logs/debug-logs.txt');
+    } catch (err) {
+      return;
     }
   }
 
@@ -139,30 +93,40 @@ export class customLogger implements LoggerService {
         return;
       }
 
-      await fsPromises.access('./logs');
+      await this.createNecessaryFolder('./logs');
+
       await fsPromises.writeFile(
         './logs/verbose-logs.txt',
-        message + `Level: verbose\n\n`,
+        message + `Type: Verbose\n\n`,
         { flag: 'a' },
       );
 
-      const fileStats = await fsPromises.lstat('./logs/verbose-logs.txt');
-      const fileSize = (fileStats.size / 1024).toFixed(2);
+      await this.rotateFilesBySize('./logs/verbose-logs.txt');
+    } catch (err) {
+      return;
+    }
+  }
 
-      if (fileSize > process.env.MAX_LOGFILE_SIZE) {
-        fsPromises.rename(
-          './logs/verbose-logs.txt',
-          `./logs/verbose-logs_${Date.now()}.txt`,
+  async createNecessaryFolder(path) {
+    try {
+      await fsPromises.access(path);
+    } catch {
+      await fsPromises.mkdir(path);
+    }
+  }
+
+  async rotateFilesBySize(path: string) {
+    try {
+      const fileSize = (await fsPromises.lstat(path)).size;
+
+      if (fileSize / 1024 >= +process.env.MAX_LOGFILE_SIZE) {
+        await fsPromises.rename(
+          path,
+          path.split('.txt')[0] + `_${Date.now()}.txt`,
         );
       }
-    } catch {
-      await fsPromises.mkdir('./logs');
-      await fsPromises.access('./logs');
-      await fsPromises.writeFile(
-        './logs/verbose-logs.txt',
-        message + `Level: verbose\n\n`,
-        { flag: 'a' },
-      );
+    } catch (error) {
+      return;
     }
   }
 }
--- src/logger/customLoggerMiddleware.ts
+++ src/logger/customLoggerMiddleware.ts
@@ -24,42 +24,46 @@ export class customLoggerMiddleware implements NestMiddleware {
     }
 
     response.on('finish', async () => {
-      const resStatusCode = response.statusCode;
-      const resContentLength = response.get('content-length');
-      const rawDate = new Date();
-      const dateToLog = `Day: ${rawDate.getDate()} Month: ${
-        rawDate.getMonth() + 1
-      } Year: ${rawDate.getFullYear()} Time: ${rawDate.getHours()}:${rawDate.getMinutes()}:${rawDate.getSeconds()}`;
+      try {
+        const resStatusCode = response.statusCode;
+        const resContentLength = response.get('content-length');
+        const rawDate = new Date();
+        const dateToLog = `Day: ${rawDate.getDate()} Month: ${
+          rawDate.getMonth() + 1
+        } Year: ${rawDate.getFullYear()} Time: ${rawDate.getHours()}:${rawDate.getMinutes()}:${rawDate.getSeconds()}`;
 
-      if (resStatusCode < 400) {
-        if (warnCodes.includes(resStatusCode)) {
-          await this.logger.warn(
-            `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nRequest time: ${dateToLog}\n`,
-          );
+        if (resStatusCode < 400) {
+          if (warnCodes.includes(resStatusCode)) {
+            await this.logger.warn(
+              `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nRequest time: ${dateToLog}\n`,
+            );
+          } else {
+            await this.logger.log(
+              `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nRequest time: ${dateToLog}\n`,
+            );
+          }
         } else {
-          await this.logger.log(
-            `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nRequest time: ${dateToLog}\n`,
+          await this.logger.error(
+            `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nError message: ${response.statusMessage}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nRequest time: ${dateToLog}\n`,
           );
         }
-      } else {
-        await this.logger.error(
-          `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nError message: ${response.statusMessage}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nRequest time: ${dateToLog}\n`,
-        );
-      }
 
-      if (+process.env.LOG_LEVEL >= 4) {
-        await this.logger.debug(
-          `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nContent-length: ${resContentLength}\nUser agent: ${userAgent}\nIp: ${reqIp}\nRequest time: ${dateToLog}\n`,
-        );
-      }
+        if (+process.env.LOG_LEVEL >= 4) {
+          await this.logger.debug(
+            `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nContent-length: ${resContentLength}\nUser agent: ${userAgent}\nIp: ${reqIp}\nRequest time: ${dateToLog}\n`,
+          );
+        }
 
-      if (+process.env.LOG_LEVEL === 5) {
-        const requestHeaders = JSON.stringify(request.headers);
-        const responseHeaders = JSON.stringify(response.getHeaders());
+        if (+process.env.LOG_LEVEL === 5) {
+          const requestHeaders = JSON.stringify(request.headers);
+          const responseHeaders = JSON.stringify(response.getHeaders());
 
-        await this.logger.verbose(
-          `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nRequest headers: ${requestHeaders}\nResponse headers: ${responseHeaders}\nContent-length: ${resContentLength}\nUser agent: ${userAgent}\nIp: ${reqIp}\nRequest time: ${dateToLog}\n`,
-        );
+          await this.logger.verbose(
+            `Method: ${reqMethod}\nStatusCode: ${resStatusCode}\nUrl: ${reqUrl}\nRequest params: ${reqParams}\nRequest body: ${reqBody}\nRequest headers: ${requestHeaders}\nResponse headers: ${responseHeaders}\nContent-length: ${resContentLength}\nUser agent: ${userAgent}\nIp: ${reqIp}\nRequest time: ${dateToLog}\n`,
+          );
+        }
+      } catch (err) {
+        return;
       }
     });
 
